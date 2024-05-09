37950382de7fa30e6e89acf748d08e27421e1661
ValeriyVinn <valeriy.all@gmail.com>
2024-05-09 14:46:04 +0300

    Added Dropdown component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Counter from "./components/Counter/Counter";
+import Dropdown from "./components/Dropdown/Dropdown";
 
 const App = () => {
   return (
     <>
+    <h1>State of Component</h1>
       <Counter initialValue={1234}/>
+      <Dropdown/>
     </>
   );
 };
