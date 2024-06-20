460c51fdb86587544b33ad6b71915397f894693d
ValeriyVinn <valeriy.all@gmail.com>
2024-06-20 13:52:14 +0300

    Added src/styles/.....scss

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import './TodoList.css';
+// import './TodoList.css';
+import classNames from 'classnames';
+import './TodoList.scss';
 
-const TodoList = ({ todos, onDeleteTodo }) => {
-  return (
-    <ul className="TodoList">
-      <h2 className="TodoList__title">Todo List</h2>
+// const TodoList = ({ todos, onDeleteTodo }) => {
+//   return (
+//     <ul className="TodoList">
+//       <h2 className="TodoList__title">Todo List</h2>
 
-      {todos.map(({ id, text }) => (
-        <li key={id} className="TodoList__item">
-          <p className="TodoList__text">{text}</p>
-          <button className="TodoList__button" onClick={() => onDeleteTodo(id)}>
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
-};
+//       {todos.map(({ id, text }) => (
+//         <li key={id} className="TodoList__item">
+//           <p className="TodoList__text">{text}</p>
+//           <button className="TodoList__button" onClick={() => onDeleteTodo(id)}>
+//             Delete
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default TodoList;
+
+const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
+  <ul className="TodoList">
+    {todos.map(({ id, text, completed }) => (
+      <li
+        key={id}
+        className={classNames('TodoList__item', {
+          'TodoList__item--completed': completed,
+        })}
+      >
+        <input
+          type="checkbox"
+          className="TodoList__checkbox"
+          checked={completed}
+          onChange={() => onToggleCompleted(id)}
+        />
+        <p className="TodoList__text">{text}</p>
+        <button
+          type="button"
+          className="TodoList__btn"
+          onClick={() => onDeleteTodo(id)}
+        >
+          Удалить
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 
 export default TodoList;
